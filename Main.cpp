55d8f42202b235b6cb2048808c22ae2cdaa85889
ARandomFurry <chaoszinc@mindspring.com>
2014-05-14 05:52:20 -0600

    Added Main.cpp and Main.hpp to start project.

---
+++ Main.cpp
@@ -0,0 +1,8 @@
+/* Main.cpp
+    Contains the project's 'main' function, nothing else.
+*/#include "Main.hpp"
+
+int main(){
+    cout<<"Hello world!"<<endl;
+    return 0;
+}
---
+++ Main.hpp
@@ -0,0 +1,17 @@
+#ifndef WORLD_EDITOR_MAIN_HPP
+#define WORLD_EDITOR_MAIN_HPP
+/// Defines.
+/// Libraries.
+// Standard Library.
+#include <iostream>
+// Boost.
+// Other Libraries.
+/// Namespaces.
+using namespace std;
+/// Class Forward Declarations.
+/// Enumerations.
+/// Structs.
+/// Typedefs.
+/// Globals.
+/// Project Files.
+#endif // WORLD_EDITOR_MAIN_HPP
